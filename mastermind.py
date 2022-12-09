83d258a2a0850537fdf6836eb3077b3328384b4a
Alice <darondeau.alice@gmail.com>
2022-12-09 10:57:37 +0100

    première fonction : comparaison de deux tableaux de strings

--- mastermind.py
+++ mastermind.py
@@ -1,1 +1,18 @@
 #Jeu Mastermind 
+
+refArray = ["blue", "yellow", "red", "green"]
+secret_code = ["yellow", "green"]
+
+def check_possible_colors(refArray, secret_code) :
+    check = 0
+    for i in refArray :
+        for j in secret_code :
+            if i == j : 
+                check += 1
+    if check == 2 :
+        print("true") 
+    else :
+        print("false")
+
+check_possible_colors(refArray, secret_code)
+
