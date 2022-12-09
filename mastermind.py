741d76be3bf1fa9ad98202c59a64977489afd0f2
Alice <darondeau.alice@gmail.com>
2022-12-09 11:41:07 +0100

    Jeu fonctionnel pour 12 essais de la part du user

--- mastermind.py
+++ mastermind.py
@@ -1,7 +1,8 @@
 #Jeu Mastermind 
 
 refArray = ["blue", "yellow", "red", "green"]
-secret_code = ["yellow", "green"]
+secret_code = ["yellow", "gray"]
+choicePlayer = []
 
 def check_possible_colors(refArray, secret_code) :
     check = 0
@@ -10,9 +11,30 @@ def check_possible_colors(refArray, secret_code) :
             if i == j : 
                 check += 1
     if check == 2 :
-        print("true") 
+        return 2 == check 
     else :
-        print("false")
+        return 0 == check
 
-check_possible_colors(refArray, secret_code)
+def check_combinaison(secret_code, choicePlayer):
+   return secret_code == choicePlayer
+
+def initiate_game():
+    choicePlayer = input("Guess two colors : ").split(" ")
+    chances = 1
+    if check_possible_colors(refArray, secret_code) == True :
+            while ((check_combinaison(secret_code, choicePlayer) == False) and (chances < 12)) :
+                choicePlayer = input("Guess again : ").split(" ") 
+                chances += 1
+            if chances == 12: 
+                print("You loose !")
+                return
+            else :
+                print("You won !")
+                return
+    else :
+        print("The secret code is invalide.")
+        return
+
+initiate_game() 
+
 
